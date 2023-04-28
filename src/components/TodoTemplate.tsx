27ba0c06b03f73bc6c198c5fe131eaa641a1176a
Heesuc <gmltjr3016@naver.com>
2023-04-28 22:29:07 +0900

    Add TodoTemplate

---
+++ src/components/TodoTemplate.tsx
@@ -0,0 +1,16 @@
+import './TodoTemplate.scss';
+
+type TodoTemplateProps = {
+    children: React.ReactNode;
+}
+
+const TodoTemplate = ( {children}: TodoTemplateProps  ) => {
+    return (
+        <div className='TodoTemplate'>
+            <div className='app-title'>일정 관리</div>
+            <div className='content'>{children}</div>
+        </div>
+    )
+}
+
+export default TodoTemplate;
